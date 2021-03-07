2cbf0bf9d53ef80c34c573b05e21a325657aa79d
David Anker <dave.anker@gmail.com>
2021-03-07 21:17:58 +1100

    fastapi

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ import pandas as pd #for data transformation before feeding data into model?
 
 app = FastAPI()
 
-nn_model = load('../models/pytorch_beer_pred.pt')
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
