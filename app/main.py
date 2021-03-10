26a3a4c94c5e591da4cc86ba3a1a67124f522257
David Anker <dave.anker@gmail.com>
2021-03-10 20:52:06 +1100

    heroku

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-#from joblib import load #only if model was saved using joblib
+#from joblib import load
 import pandas as pd
 import torch
 import torchvision
@@ -15,11 +15,15 @@ class PytorchMultiClass(nn.Module):
         super(PytorchMultiClass, self).__init__()
         
         self.layer_1 = nn.Linear(num_features, 32)
-        self.layer_out = nn.Linear(32, 104) #Number of classes in target variable?
+        self.layer_2 = nn.Linear(32,16)
+        self.layer_3 = nn.Linear(16,8)
+        self.layer_out = nn.Linear(8, 104)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.dropout(F.relu(self.layer_1(x)), training=self.training)
+        x = F.dropout(F.relu(self.layer_2(x)), training=self.training)
+        x = F.dropout(F.relu(self.layer_3(x)), training=self.training)
         x = self.layer_out(x)
         return self.softmax(x)
 
@@ -35,8 +39,8 @@ def healthcheck():
 
 @app.get("/model/architecture")
 def architecture():
-    #model = models.densenet121(pretrained=True)
-    model = torch.load("../models/beer_pred.pt")
+    model = models.densenet121(pretrained=True)
+    #model = torch.load("../models/beer_pred.pt", encoding='ascii')
     return model
 
 def format_features(brewery_name: str, review_aroma: int, review_appearance: int, review_palate: int, review_taste: int):
@@ -54,7 +58,8 @@ def predict(brewery_name: int=None, review_aroma: int=None, review_appearance: i
     # convert row to data
     features = Tensor([features])
     # make prediction
-    model = torch.load("../models/beer_pred.pt")
+    #model = torch.load("../models/beer_pred.pt")
+    model = models.densenet121(pretrained=True)
     yhat = model(features)
     # retrieve numpy array
     yhat = yhat.detach().numpy()
